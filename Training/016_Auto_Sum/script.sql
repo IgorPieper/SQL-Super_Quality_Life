c1d850490afcc5f818aa1b1f630a4821e9792c0b
Igor Pieper <91740800+IgorPieper@users.noreply.github.com>
2025-12-28 22:15:57 +0100

    Create script.sql

---
+++ Training/016_Auto_Sum/script.sql
@@ -0,0 +1,16 @@
+SELECT
+  EXTRACT(YEAR  FROM orderdate) AS "year",
+  EXTRACT(MONTH FROM orderdate) AS "month",
+  EXTRACT(DAY   FROM orderdate) AS "day",
+  SUM(ol.quantity)
+FROM orderlines AS ol
+GROUP BY
+  ROLLUP (
+    EXTRACT(YEAR  FROM orderdate),
+    EXTRACT(MONTH FROM orderdate),
+    EXTRACT(DAY   FROM orderdate)
+  )
+ORDER BY
+  EXTRACT(YEAR  FROM orderdate),
+  EXTRACT(MONTH FROM orderdate),
+  EXTRACT(DAY   FROM orderdate);
